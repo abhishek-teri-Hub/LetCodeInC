b6ea56abdef5c05e4f54b2fcd0fbd544d03ce21b
Abhishek Singh <abhishek.teri@gmail.com>
2026-09-18 15:10:49 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int x=20;
+    printf("Hello, World!\n");
+    int y=30;
+
+    printf("The value of x is: %d\n", x);
+    printf("The value of y is: %d\n", y);
+    return 0;
+}
